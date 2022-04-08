0cd197db709e1b059e0ca5f84b46cb4723b973d5
Will Taylor <william_taylor@brown.edu>
2022-04-07 22:27:31 -0400

    Add cesium isotopes

---
+++ src/thesis/isotopes/cesium.py
@@ -0,0 +1,49 @@
+ISOTOPES = \
+{
+    "Cs135": \
+    {
+        "label": "$^{135}$Cs",
+        "parents": \
+        [
+            {"Xe135": "RadioactiveDecay"},
+        ],
+        "processes": \
+        {
+            "RadioactiveDecay": \
+            {
+                "children": \
+                {
+                    "Ba135": \
+                    {
+                        "half_life_days": 2.3e6*365,
+                        "mode": "$\beta^{-}$",
+                        "spectrum": "xebox_50k_cs135_decay_444282737_flat.root",
+                    },
+                },
+            },
+        },
+    },
+    "Cs137": \
+    {
+        "label": "$^{137}$Cs",
+        "parents": \
+        [
+            {"Xe137": "RadioactiveDecay"},
+        ],
+        "processes": \
+        {
+            "RadioactiveDecay": \
+            {
+                "children": \
+                {
+                    "Ba137": \
+                    {
+                        "half_life_days": 30.07*365,
+                        "mode": "$\beta^{-}$",
+                        "spectrum": "xebox_50k_cs137_decay_133316960_flat.root",
+                    },
+                },
+            },
+        },
+    },
+}
